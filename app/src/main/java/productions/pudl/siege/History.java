01a7aaaa0c8d9e832442f06b6db63ddbb9b60b83
Unknown <laurence.ure@gmail.com>
2018-03-26 20:24:00 +0100

    Stashing changes to revert to attempt to fix things that dont work

--- app/src/main/java/productions/pudl/siege/History.java
+++ app/src/main/java/productions/pudl/siege/History.java
@@ -3,25 +3,25 @@ package productions.pudl.siege;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.IdRes;
 
 import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnTabSelectListener;
 
-public class Home extends AppCompatActivity {
+public class History extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_history);
 
         BottomBar navigation = findViewById(R.id.bottomBar);
 
         final Intent statsIntent = new Intent(this, Stats.class);
-        final Intent historyIntent = new Intent(this, History.class);
         final Intent profileIntent = new Intent(this, Profile.class);
         final Intent savedIntent = new Intent(this, Saved.class);
+        final Intent homeIntent = new Intent(this, Home.class);
 
         navigation.setOnTabSelectListener(new OnTabSelectListener() {
             @Override
@@ -30,9 +30,10 @@ public class Home extends AppCompatActivity {
                 if (tabId == R.id.navigation_home) {
                     // The tab with id R.id.tab_favorites was selected,
                     // change your content accordingly.
+                    startActivity(homeIntent);
                 }
                 else if (tabId == R.id.navigation_history){
-                    startActivity(historyIntent);
+
                 }
                 else if (tabId == R.id.navigation_profile) {
                     startActivity(profileIntent);
