6fe92003a1a24f075847290ba6abe985052e97d6
Unknown <laurence.ure@gmail.com>
2018-04-02 17:32:53 +0100

    Got a simple search working using animals as example
    Maybe revisit previous commit and try widget.v7.searchview instead of <searchview> as that was crash error

--- app/src/main/java/productions/pudl/siege/Search.java
+++ app/src/main/java/productions/pudl/siege/Search.java
@@ -12,11 +12,22 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.content.Context;
 
-public class Search extends Fragment implements AdapterView.OnItemSelectedListener
+import java.util.ArrayList;
+import java.util.List;
+
+public class Search extends Fragment implements AdapterView.OnItemSelectedListener, SearchView.OnQueryTextListener
 {
+    SearchView search;
+    ListView list;
+    ListViewAdapter adapter2;
+    String[] animalNameList;
+    ArrayList<AnimalNames> arraylist = new ArrayList<AnimalNames>();
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState)
@@ -25,30 +36,35 @@ public class Search extends Fragment implements AdapterView.OnItemSelectedListen
 
         Spinner spinner = (Spinner) view.findViewById(R.id.spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(), R.array.platform_array, android.R.layout.simple_spinner_item);
-        //ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, R.array.platform_array);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setPrompt("Platform");
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
 
-        SearchManager searchManager = (SearchManager) getContext().getSystemService(Context.SEARCH_SERVICE);
-        SearchView searchView = (SearchView) view.findViewById(R.id.searchView);
-        searchView.setIconified(false);
-        searchView.setIconifiedByDefault(false);
-        //searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-        searchView.requestFocus();
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener()
-        {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return true;
-            }
+        animalNameList = new String[]{"Lion", "Tiger", "Dog",
+                "Cat", "Tortoise", "Rat", "Elephant", "Fox",
+                "Cow","Donkey","Monkey", "X", "Y", "Z", "Q"};
 
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                return false;
-            }
-        });
+        // Locate the ListView in listview_main.xml
+        list = (ListView) view.findViewById(R.id.listview);
+
+        for (String anAnimalNameList : animalNameList) {
+            AnimalNames animalNames = new AnimalNames(anAnimalNameList);
+            // Binds all strings into an array
+            arraylist.add(animalNames);
+        }
+
+        // Pass results to ListViewAdapter Class
+        adapter2 = new ListViewAdapter(getContext(), arraylist);
+
+        // Binds the Adapter to the ListView
+        list.setAdapter(adapter2);
+
+        // Locate the EditText in listview_main.xml
+        search = (SearchView) view.findViewById(R.id.searchView);
+        search.setIconified(false);
+        search.setIconifiedByDefault(false);
+        search.setOnQueryTextListener(this);
 
         return view;
     }
@@ -62,5 +78,19 @@ public class Search extends Fragment implements AdapterView.OnItemSelectedListen
     @Override
     public void onNothingSelected(AdapterView<?> adapterView)
     {
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        search.setIconified(true); //minimise search field
+        //display stats instead of listview
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        String text = newText;
+        adapter2.filter(text);
+        return false;
     }
 }
